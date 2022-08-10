d943960dfe29dd46fd5d4284c51f353a8ede5e06
Sylvester Agbo <105360514+slybullion@users.noreply.github.com>
2022-08-10 20:48:41 +0100

    Revert "user authentication"

--- Apiproject/django_project/applogger.py
+++
@@ -1,9 +0,0 @@
-import os
-import logging
-from logging.config import fileConfig
-import logging.handlers as handlers
-import time
-
-os.makedirs("logs", exist_ok=True)
-fileConfig('logging_config.ini')
-logger = logging.getLogger()
