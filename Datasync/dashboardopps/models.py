efc8c8c6b78e1985d39edb0a38430ca7c066b6c9
Oluwatosin Peters <peterstosin556@gmail.com>
2022-08-10 17:49:25 +0100

    added remove duplicate function

--- Datasync/dashboardopps/models.py
+++ Datasync/dashboardopps/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import pandas as pd
 
 # Create your models here.
+class Removeduplicate():
+    orders = pd.read_excel(r'C:\Users\oluwatosin.peters\Downloads\ZONAL_ARCHIVES_AND_BRANCHES_ACCOMODATED\SOUTH SOUTH ZONE ARCHIVES AND BRANCHES.xlsx')
+#print(orders)
+    orders.drop_duplicates()
