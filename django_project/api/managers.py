fc0f496ee9fd219a8d2a2ab0cfa9f67938a178b3
wemimofolayan <68654903+wemimofolayan@users.noreply.github.com>
2022-08-11 18:19:16 +0100

    authentication commit
    login and signup authentication

---
+++ django_project/api/managers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model where the email address is the unique identifier
+    and has an is_admin field to allow access to the admin app 
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("The email must be set"))
+        if not password:
+            raise ValueError(_("The password must be set"))
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(email, password, **extra_fields)
